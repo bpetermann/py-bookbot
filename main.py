ee51e92123e6cf79968ec655e4935c74ba25064d
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-09-23 16:46:37 +0200

    Add count words and letters

---
+++ main.py
@@ -0,0 +1,41 @@
+def count_words(text):
+    words_count = 0
+    for word in text:
+        words_count += 1
+    return words_count
+
+def count_letters(text):
+    letters_dict = {}
+    for word in text:
+        for char in word:
+            index = char.lower()
+            if index in letters_dict and index.isalpha():
+                letters_dict[index] += 1
+            elif index.isalpha():
+                letters_dict[index] = 1
+    return letters_dict
+
+def sort_letters(letters):
+    letters_list = []
+    for letter in letters:
+        letters_list.append((letter, letters[letter]))
+    letters_list.sort(key=lambda x:int(x[1]), reverse=True)
+    return letters_list
+
+def print_report(count, letters):
+    print("--- Begin report ---")
+    print(f"{count} words found in the document")
+    print("")
+    for letter in letters:
+        print(f"The '{letter[0]}' character was found {letter[1]} times")
+    
+    print("")
+    print("--- End report ---")
+
+with open("books/frankenstein.txt") as f:
+    file_content = f.read()
+    text = file_content.split()
+    word_count = count_words(text)
+    sorted_letters = sort_letters(count_letters(text))
+
+    print_report(word_count, sorted_letters)
